7dd1e2c791cfe71543eab9acaca1f8b497c6f212
Adrian McEwen <adrianm@mcqn.com>
2013-12-24 22:47:04 +0000

    Vanilla Rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Queuestreamer::Application.config.secret_key_base = 'c00dd6057463894cc29b9d62c765e4ca04752ad2aa569493d9e10a63a1db32d776814befbaaa29062664ab0f9349fc181274650a93565a56b8ffec2717ca1724'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Queuestreamer::Application.config.session_store :cookie_store, key: '_queuestreamer_session'
